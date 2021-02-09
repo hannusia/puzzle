224664517ba4d80f538a8ea158e8bb4771368cbf
hannusia <hanna.yershova@ucu.edu.ua>
2021-02-09 03:17:09 +0200

    add puzzle module

--- puzzle.py
+++ puzzle.py
@@ -1,11 +1,23 @@
-
+"""
+Check if game board fits the rules.
+https://github.com/hannusia/puzzle.git
+"""
 def check_lines(board: list) -> bool:
+    """
+    Check if lines fit to rules.
+    >>> check_lines(['****1****', '*** 1****', '**2  ****', '*   3****',
+         ' 4       ', '5   5   *', '  6    **', ' 7    ***', '8    ****'])
+    False
+    >>> check_lines(['****1****', '***12****', '**123****', '*1234****',
+         '123456789', '23456789*', '3456789**', '456789***', '56789****'])
+    True
+    """
     lines = {0: [], 1: [], 2: [], 3: [], 4: [],
              5: [], 6: [], 7: [], 8: []}
     for i in range(len(board)):
         for j in board[i]:
             if j in '123456789':
-                lines[i] = [j]
+                lines[i].append(j)
     for i in lines:
         if len(set(lines[i])) != len(lines[i]):
             return False
@@ -14,9 +26,12 @@ def check_lines(board: list) -> bool:
 
 def check_rows(board: list) -> bool:
     """
-    Check if the rows fit to rules.
-    >>> check_rows(["*******","***1***","****3**","**4*1*9","*****8*","*61**35",\
-        "3**8***","*******","**2*5**","***2***","*******"])
+    Check if rows fit to rules.
+    >>> check_rows(['****1****', '*** 1****', '**2  ****', '*   3****',
+         ' 4       ', '5   5   *', '  6    **', ' 7    ***', '8    ****'])
+    False
+    >>> check_rows(['****1****', '***12****', '**123****', '*1234****',
+         '123456789', '23456789*', '3456789**', '456789***', '56789****'])
     True
     """
     rows = {0: [], 1: [], 2: [], 3: [], 4: [],
@@ -24,7 +39,7 @@ def check_rows(board: list) -> bool:
     for i in board:
         for j in range(len(i)):
             if i[j] in '123456789':
-                rows[j] = [i[j]]
+                rows[j].append(i[j])
     for i in rows:
         if len(set(rows[i])) != len(rows[i]):
             return False
@@ -33,14 +48,17 @@ def check_rows(board: list) -> bool:
 
 def check_colors(board: list) -> bool:
     """
-    Check if the colors fit to rules.
-    >>> check_colors(["*******","***1***","****3**","**4*1*9","*****8*","*61**35",\
-        "3**8***","*******","**2*5**","***2***","*******"])
+    Check if colors fit to rules.
+    >>> check_colors(['****1****', '*** 1****', '**2  ****', '*   3****',
+         ' 4       ', '5   5   *', '  6    **', ' 7    ***', '8    ****'])
+    False
+    >>> check_colors(['****1****', '***12****', '**123****', '*1234****',
+         '123456789', '23456789*', '3456789**', '456789***', '56789****'])
     True
     """
     colors = {0: [], 1: [], 2: [], 3: [], 4: []}
     for k in range(5):
-        i = 9 - k
+        i = 8 - k
         j = k
         if board[i][j] in '123456789':
             colors[k].append(board[i][j])
@@ -69,5 +87,11 @@ def check_colors(board: list) -> bool:
 def validate_board(board: list) -> bool:
     """
     Check if board is ready for a game.
+    >>> validate_board(['****1****', '*** 1****', '**2  ****', '*   3****',
+         ' 4       ', '5   5   *', '  6    **', ' 7    ***', '8    ****'])
+    False
+    >>> validate_board(['****1****', '***12****', '**123****', '*1234****',
+         '123456789', '23456789*', '3456789**', '456789***', '56789****'])
+    True
     """
     return check_lines(board) and check_rows(board) and check_colors(board)
